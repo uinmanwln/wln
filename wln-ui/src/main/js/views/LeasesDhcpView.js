3c395ba225a559e14dc1f24ebce63a98a3d07c23
uinman <sreib@mail.ru>
2018-04-13 00:51:41 +0700

    create module

--- wln-ui/src/main/js/views/LeasesDhcpView.js
+++ wln-ui/src/main/js/views/LeasesDhcpView.js
@@ -7,20 +7,23 @@ import ChevronLeft from 'material-ui/svg-icons/navigation/chevron-left';
 
 export default class LeasesDhcpView extends Component {
     render() {
-
         const style2 = {
             height: '98vh',
             width: '100%',
             display: 'inline-block',
         };
-
-
+        var self = this;
         return (
-            <Paper   transitionEnabled={false} style={style2} zDepth={2}>
+            <Paper transitionEnabled={false} style={style2} zDepth={2}>
                 <AppBar
                     title="Аренда DHCP"
                     style={{background: '#303F9F'}}
-                    iconElementLeft={<IconButton><ChevronLeft /></IconButton>}
+                    iconElementLeft={<IconButton
+                        onClick={() =>{self.props.history.push('/');}}
+                    >
+                        <ChevronLeft />
+                    </IconButton>
+                    }
                 />
             </Paper>
         )
